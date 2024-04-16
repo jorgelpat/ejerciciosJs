434e21cda87fd6a41422689fd888b14dbb716ea3
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-04-16 12:24:51 -0500

    refactor: :construction: Se corrige ejercicio 1
    Se cambia código para visualizar con ayuda de Html

--- js/modules/ejercicio1.js
+++ js/modules/ejercicio1.js
@@ -1,22 +1,36 @@
-import prompt from "prompt-sync"
-const input = prompt()
+// import prompt from "prompt-sync"
+// const input = prompt()
 
-export default function entradaUsuario() {
-    let opc = undefined;
+// export default function entradaUsuario() {
+//     let opc = undefined;
 
-    do {
-        let data = input("Ingrese la contraseña: ")
-        console.log(contrasenaValida(data));
-        console.log(`1. Repetir la validacion\n0. Terminar`);
-        opc = +input()
+//     do {
+//         let data = input("Ingrese la contraseña: ")
+//         console.log(contrasenaValida(data));
+//         console.log(`1. Repetir la validacion\n0. Terminar`);
+//         opc = +input()
 
-    } while (opc)
+//     } while (opc)
 
+// }
+
+// function contrasenaValida(data) {
+
+//     if (data == "2Fj(jjbFsuj" || data == "eoZiugBf&g9") {
+//         return true
+//     } else return false
+// }
+
+function validarContrasena() {
+    let contrasena = document.getElementById("contrasenaInput").ariaValueMax;
+    let esValida = contrasenaValida(contrasena);
+    if (esValida) {
+        document.getElementById("resultado").innerText = "Contraseña Valida"; 
+    } else {
+        document.getElementById("resultado").innerText = "Contraseña Incorrecta";
+    }
 }
 
-function contrasenaValida(data) {
-
-    if (data == "2Fj(jjbFsuj" || data == "eoZiugBf&g9") {
-        return true
-    } else return false
+function contrasenaValida(contrasena) {
+    return contrasena === "1234" || contrasena === "4567";
 }
